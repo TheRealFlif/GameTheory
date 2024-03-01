59b163eadfa77997fc8df43d41eb05d2ef7445b5
Stefan Cronert <cronert@yahoo.com>
2024-03-01 16:11:46 +0100

    Rename of AssemblyInfo1.cs Added TitForTat strategy

---
+++ GameTheory.Logic/Entities/TitForTatStrategy.cs
@@ -0,0 +1,11 @@
+﻿namespace GameTheory.Logic.Entities;
+
+internal class TitForTatStrategy(string name) : StrategyBase(name)
+{
+    public override Choice Next(Choice opponentPreviousChoice)
+    {
+        return opponentPreviousChoice == Choice.Start
+            ? Choice.Cooperate
+            : opponentPreviousChoice;
+    }
+}
